38ea09418e4906d55a7143cc27efbf595a2ec609
Bruno Fernando Zabala Peña <109123551+burnoz@users.noreply.github.com>
2023-04-13 07:55:39 -0600

    Delete test.js

--- ESP32/test.js
+++
@@ -1,5 +0,0 @@
-let d = new Date();
-let msg = "ooo";
-let num = 5;
-
-document.body.innerHTML = "<h1> Today's date is " + d + "</h1>\n" + "<h2> msg: " + msg + "</h2>\n" + "<h2> num: " + num + "</h2>"
